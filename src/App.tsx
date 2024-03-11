45f943c8433c963ef853616c0f8e1992c65efcb2
mmariaiv <mari.ivashova@gmail.com>
2024-03-11 21:50:17 +0300

    feat/ added React LazyLoading

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,20 @@
-import {Route, Routes} from "react-router";
+import React from "react";
+import { Route, Routes } from "react-router";
 import SEO from "@utils/seo.tsx";
 import Layout from "@layout/layout";
-import Home from "@pages/home";
+const Home = React.lazy(() => import("@pages/home"));
 
 function App() {
-
   return (
     <div className={"body"}>
-        <SEO title={""} description={""} keywords={""}/>
-        <Layout>
-      <Routes>
-        <Route path={"/"} element={<Home />}/>
-      </Routes>
-        </Layout>
+      <SEO title={""} description={""} keywords={""} />
+      <Layout>
+        <Routes>
+          <Route path={"/"} element={<Home />} />
+        </Routes>
+      </Layout>
     </div>
-
-  )
+  );
 }
 
-export default App
+export default App;
